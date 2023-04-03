791fd32adb7a83bb7d0784930611a80ad234232d
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-03 14:57:29 +0700

    membuat wadah dan menggunakan namespace std

--- ActivityStructure_157/ActivityStructure_157.cpp
+++ ActivityStructure_157/ActivityStructure_157.cpp
@@ -3,38 +3,38 @@
 using namespace std;
 
 struct Mahasiswa {
-	string nim;
-	string nama;
-	string alamat;
-	int umur;
+	string	nim;
+	string	nama;
+	string	alamat;
+	int		umur;
 };
 
 int main()
 {
 	Mahasiswa mhs1, mhs2;
 
-	mhs1.nim = "20210140011";
-	mhs1.nama = "Tasya";
-	mhs1.alamat = "Semarang";
-	mhs1.umur = 20;
+	mhs1.nim	=		"20210140011";
+	mhs1.nama	=		"Tasya";
+	mhs1.alamat =		"Semarang";
+	mhs1.umur	=		20;
 
-	cout << "Masukkan NIM : ";
-	cin >> mhs2.nim;
-	cout << "Masukkan Nama : ";
-	cin >> mhs2.nama;
-	cout << "Masukkan Alamat : ";
-	cin >> mhs2.alamat;
-	cout << "Masukkan Umur : ";
-	cin >> mhs2.umur;
+	cout <<		"Masukkan NIM : ";
+	cin >>		mhs2.nim;
+	cout <<		"Masukkan Nama : ";
+	cin >>		mhs2.nama;
+	cout <<		"Masukkan Alamat : ";
+	cin >>		mhs2.alamat;
+	cout <<		"Masukkan Umur : ";
+	cin >>		mhs2.umur;
 
-	cout << "\n NIM : " << mhs1.nim;
-	cout << "\n Nama : " << mhs1.nama;
-	cout << "\n Alamat: " << mhs1.alamat;
-	cout << "\n Umur : " << mhs1.umur << endl;
+	cout << "\n NIM : "		<< mhs1.nim;
+	cout << "\n Nama : "	<< mhs1.nama;
+	cout << "\n Alamat: "	<< mhs1.alamat;
+	cout << "\n Umur : "	<< mhs1.umur << endl;
 
-	cout << "\n NIM : " << mhs2.nim;
-	cout << "\n Nama : " << mhs2.nama;
-	cout << "\n Alamat: " << mhs2.alamat;
-	cout << "\n Umur : " << mhs2.umur;
+	cout << "\n NIM : "		<< mhs2.nim;
+	cout << "\n Nama : "	<< mhs2.nama;
+	cout << "\n Alamat: "	<< mhs2.alamat;
+	cout << "\n Umur : "	<< mhs2.umur;
 
 }
