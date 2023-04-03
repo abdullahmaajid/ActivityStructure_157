f66c92488c9260becbe06d36aea8db29f90168ed
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-03 15:25:23 +0700

    membuat input mahasiswa

--- ActivityStructure_157 (3)/ActivityStructure_157 (3).cpp
+++ ActivityStructure_157 (3)/ActivityStructure_157 (3).cpp
@@ -19,5 +19,21 @@ struct Mahasiswa
 int main()
 {
 
+	Mahasiswa mhs[3];
 
+		for (int i = 0; i < 3; i++)
+		{
+			cout << "Masukkan NIM : ";
+			cin.getline(mhs[i].nim, 12);
+			cout << "Masukkan Nama :";
+			cin.getline(mhs[i].nama, 20);
+			cout << "Alamat" << endl;
+			cout << "\t Desa : ";
+			cin.getline(mhs[i].alamat.desa, 20);
+			cout << "\t Kota : ";
+			cin.getline(mhs[i].alamat.kota, 20);
+			cout << "Masukkan Umur : ";
+			cin >> mhs[i].umur;
+			cin.ignore(1, '\n');
+		}
 }
