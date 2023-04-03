f2e5eeb2ed3bbbff8d327b0a11d53e541bf90601
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-03 15:21:27 +0700

    membuat struct mahasiswa menggunakan char

--- ActivityStructure_157 (3)/ActivityStructure_157 (3).cpp
+++ ActivityStructure_157 (3)/ActivityStructure_157 (3).cpp
@@ -5,7 +5,19 @@ using namespace std;
 struct AlamatDetail
 {
 	char desa [20];
-	string kota [20];
+	char kota [20];
 };
 
+struct Mahasiswa
+{
+	char nim[12];
+	char nama[20];
+	AlamatDetail alamat;
+	int		umur;
+};
 
+int main()
+{
+
+
+}
